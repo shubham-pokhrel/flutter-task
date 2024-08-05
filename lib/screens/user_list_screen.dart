0991744bdd40d1d134ce4d78c626e08958648328
Shubham Pokhrel <contact@shubhampokhrel.com.np>
2024-08-05 21:44:40 +0545

    added a user list screen, fetch and display

---
+++ lib/screens/user_list_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:my_postviewer/models/user.dart';
+import 'package:my_postviewer/api_services/api_service.dart';
+
+class UserListScreen extends StatefulWidget {
+  @override
+  _UserListScreenState createState() => _UserListScreenState();
+}
+
+class _UserListScreenState extends State<UserListScreen> {
+  late Future<List<User>> futureUsers;
+
+  @override
+  void initState() {
+    super.initState();
+    futureUsers = fetchUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Users List'),
+      ),
+      body: Center(
+        child: FutureBuilder<List<User>>(
+          future: futureUsers,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(snapshot.data![index].name),
+                    subtitle: Text(snapshot.data![index].email),
+                    
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
+  }
+}
