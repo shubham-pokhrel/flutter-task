b750f9f410488db1bc6dab0b96492d176b22ea90
Shubham Pokhrel <contact@shubhampokhrel.com.np>
2024-08-05 16:35:56 +0545

    Integrate ApiService with PostsListScreen to fetch and display posts

--- lib/screens/posts_list_screen.dart
+++ lib/screens/posts_list_screen.dart
@@ -1,27 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:my_postviewer/models/post.dart';
+import 'package:my_postviewer/api_services/api_service.dart';
 import 'post_details_screen.dart';
 
-class PostsListScreen extends StatelessWidget {
+class PostsListScreen extends StatefulWidget {
+  @override
+  _PostsListScreenState createState() => _PostsListScreenState();
+}
+
+class _PostsListScreenState extends State<PostsListScreen> {
+  late Future<List<Post>> futurePosts;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePosts = ApiService().fetchPosts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Posts List'),
       ),
-      body: ListView.builder(
-        itemCount: 10, // This should be the length of your posts list
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Post ${index + 1}'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => PostDetailsScreen(postId: index + 1),
-                ),
-              );
-            },
-          );
+      body: FutureBuilder<List<Post>>(
+        future: futurePosts,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No posts found'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final post = snapshot.data![index];
+                return ListTile(
+                  title: Text(post.title),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PostDetailsScreen(postId: post.id),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          }
         },
       ),
     );
