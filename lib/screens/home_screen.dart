3ce372610af8121ce04f1da0188876d70c71eaeb
Shubham Pokhrel <contact@shubhampokhrel.com.np>
2024-08-05 21:45:26 +0545

    enhanced ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_postviewer/screens/user_list_screen.dart';
 import 'posts_list_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,24 +8,47 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
+        backgroundColor: Colors.teal, // Minimalistic color for the app bar
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PostsListScreen(),
-                  ),
-                );
-              },
-              child: Text('Go to Posts List'),
-            ),
-            // Additional buttons can be added here for other screens
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(16.0), // Add padding around the body
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PostsListScreen(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal, // Button background color
+                  foregroundColor: Colors.white, // Button text color
+                ),
+                child: Text('Go to Posts List'),
+              ),
+              SizedBox(height: 16.0), // Add space between buttons
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => UserListScreen(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal, // Button background color
+                  foregroundColor: Colors.white, // Button text color
+                ),
+                child: Text('Go to User List'),
+              ),
+            ],
+          ),
         ),
       ),
     );
