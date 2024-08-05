57b5ee7bc84fb5c59058868d7012fcb891f20bd6
Shubham Pokhrel <contact@shubhampokhrel.com.np>
2024-08-05 23:10:40 +0545

    Change homescreen appbar name

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Post Viewer'),
         backgroundColor: Colors.teal, // Minimalistic color for the app bar
       ),
       body: Center(
